4dad5ea3b48c55c245a3d168db7bdaf117d48b94
tuminzee <t.sheth33@gmail.com>
2024-05-21 13:01:50 +0530

    add mac hardware acceleration, shift approach from file to dir

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python
 import os
-import sys
 
 
-def help(name: str):
-    print("Sup!")
-    print('You need to write: {name} "FILENAME.mp4"'.format(name=name))
-    print("Then ffmpeg will convert your mp4 to mov for usage in davinci resolve!")
-
+def convert_to_mp4(path: str):
+    mp4_path = path.replace(".MOV", ".mp4")
+    command = "ffmpeg -i {path} -vcodec h264_videotoolbox -pix_fmt yuv420p -b:v 5000k -acodec aac -b:a 320k {mp4_path}".format(path=path, mp4_path=mp4_path)
+    os.system(command)
+    
+# def convert_to_MOV(path: str):
+#     MOV_path = path.replace(".mp4", ".MOV")
+#     command = "ffmpeg -i {path} -vcodec h264_videotoolbox -pix_fmt yuv420p -b:v 5000k -acodec aac -b:a 320k {MOV_path}".format(path=path, MOV_path=MOV_path)
+#     os.system(command)
 
 def main():
-    args = sys.argv
-    name = args[0]
-    path = args[1]
+    dir = ""
     
-    if path == "--help" or path == "-h":
-        help(name)
-    else:
-        mov_path = path.replace(".mp4", ".mov")
-        os.system("ffmpeg -i {path} -vcodec mjpeg -q:v 2 -acodec pcm_s16be -q:a 0 -f mov {mov_path}".format(path=path, mov_path=mov_path))
-
-
+    for file in os.listdir(dir):
+        if file.endswith(".MOV"):
+            path = os.path.join(dir, file)
+            convert_to_mp4(path)
+        # if file.endswith(".mp4"):
+        #     path = os.path.join(dir, file)
+        #     convert_to_MOV(path)
+            
+            
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception:
-        print("Wrong input, please type again.")
-        main()
+    main()
